27609a4577b2c644d130c081db12a8e6e3bea805
tsd03150 <tsd03150@naver.com>
2022-02-24 11:43:29 +0900

    220224_1260_1 commit :)

--- main/src/com/kaveloper/algorithm/Main_1260.java
+++ main/src/com/kaveloper/algorithm/Main_1260.java
@@ -1,2 +1,9 @@
-package com.kaveloper.algorithm;public class Main_1260 {
+package com.kaveloper.algorithm;
+
+class Main_1260 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Kaveloper world");
+    }
+
 }
