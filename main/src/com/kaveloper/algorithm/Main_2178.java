7e1aec1bbf521fa7cc007486b80465fb75454407
tsd03150 <tsd03150@naver.com>
2022-02-24 17:29:26 +0900

    220224_2178_2 commit

---
+++ main/src/com/kaveloper/algorithm/Main_2178.java
@@ -0,0 +1,71 @@
+package com.kaveloper.algorithm;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Graph {
+    int x;
+    int y;
+
+    Graph(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Main_2178 {
+    static int n;
+    static int m;
+    static char[][] board;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int answer;
+
+    public void BFS(int L) {
+        Queue<Graph> Q = new LinkedList<>();
+        Q.offer(new Graph(0, 0));
+        board[0][0] = 0;
+
+        while (!Q.isEmpty()) {
+            int len = Q.size();
+
+            for (int i = 0; i < len; i++) {
+                Graph ob = Q.poll();
+                for (int j = 0; j < 4; j++) {
+                    int nx = ob.x + dx[j];
+                    int ny = ob.y + dy[j];
+
+                    if (nx == n-1 && ny == m-1) {
+                        answer = L+1;
+                        return;
+                    }
+
+                    if ((0 <= nx && nx < n) && (0 <= ny && ny < m) && board[nx][ny] == '1') {
+                        board[nx][ny] = '0';
+                        Q.offer(new Graph(nx, ny));
+                    }
+                }
+            }
+            L++;
+        }
+    }
+
+    public static void main(String[] args) {
+        Main_2178 T = new Main_2178();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        board = new char[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String str = kb.next();
+            for (int j = 0; j < m; j++) {
+                board[i][j] = str.charAt(j);
+            }
+        }
+
+        T.BFS(1);
+        System.out.println(answer);
+    }
+}
---
+++ main/src/com/kaveloper/algorithm/Main_2178_DFS.java
@@ -0,0 +1,56 @@
+package com.kaveloper.algorithm;
+
+import java.util.Scanner;
+
+class Main_2178_DFS {
+    static int n;
+    static int m;
+    static int[][] ch;
+    static char[][] board;
+    static int answer = Integer.MAX_VALUE;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public void DFS(int x, int y, int sum) {
+        if (sum > answer) {
+            return;
+        }
+
+        if (x == n && y == m) {
+            answer = Math.min(answer, ch[x][y]);
+        } else {
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if ((0 < nx && nx <= n) && (0 < ny && ny <= m) && board[nx][ny] == '1') {
+                    ch[nx][ny] = ch[x][y] + 1;
+                    board[nx][ny] = '0';
+                    DFS(nx, ny, ch[nx][ny]);
+                    board[nx][ny] = '1';
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Main_2178_DFS T = new Main_2178_DFS();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        board = new char[n + 1][m + 1];
+        ch = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            String str = kb.next();
+            for (int j = 1; j <= m; j++) {
+                board[i][j] = str.charAt(j - 1);
+            }
+        }
+
+        ch[1][1] = 1;
+        T.DFS(1, 1, 0);
+        System.out.println(answer);
+    }
+
+}
