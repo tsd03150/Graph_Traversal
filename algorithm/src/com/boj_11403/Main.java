13c18734afe53b773b704affa1f920686e735167
tsd03150 <tsd03150@naver.com>
2022-03-01 17:20:52 +0900

    220301_11403_4 commit

---
+++ algorithm/src/com/boj_11403/Main.java
@@ -0,0 +1,35 @@
+package com.boj_11403;
+
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[][] board = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = kb.nextInt();
+            }
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (board[i][k] == 1 && board[k][j] == 1) {
+                        board[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
