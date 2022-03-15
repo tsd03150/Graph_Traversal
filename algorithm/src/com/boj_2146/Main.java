2fc57f6d0fdae225d9ef11c86f8389aff81f161f
tsd03150 <tsd03150@naver.com>
2022-03-15 10:45:47 +0900

    220315_2146_1 commit

--- algorithm/src/com/boj_2146/Main.java
+++ algorithm/src/com/boj_2146/Main.java
@@ -7,10 +7,12 @@ import java.util.Scanner;
 class Graph {
     int x;
     int y;
+    int cnt;
 
-    Graph(int x, int y) {
+    Graph(int x, int y, int cnt) {
         this.x = x;
         this.y = y;
+        this.cnt = cnt;
     }
 }
 
@@ -20,10 +22,12 @@ class Main {
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
     static int num = 2;
+    static int[][] visit;
+    static int answer;
 
-    public int BFS(int L, int x, int y, int name) {
+    public int BFS(int x, int y, int name) {
         Queue<Graph> Q = new LinkedList<>();
-        Q.offer(new Graph(x, y));
+        Q.offer(new Graph(x, y, 0));
         int[][] ch = new int[n][n];
         ch[x][y] = 1;
 
@@ -31,29 +35,33 @@ class Main {
             int len = Q.size();
             for (int i = 0; i < len; i++) {
                 Graph ob = Q.poll();
+                if (ob.cnt > answer) {
+                    continue;
+                }
+
+                if (board[ob.x][ob.y] > 0 && board[ob.x][ob.y] != name) {
+                    return ob.cnt - 1;
+                }
+
                 for (int j = 0; j < 4; j++) {
                     int nx = ob.x + dx[j];
                     int ny = ob.y + dy[j];
 
-                    if (0 <= nx && nx < n && 0 <= ny && ny < n) {
-                        if (board[nx][ny] > 0 && board[nx][ny] != name && ch[nx][ny] == 0) {
-                            return L;
-                        } else {
-                            Q.offer(new Graph(nx, ny));
-                            ch[nx][ny] = 1;
-                        }
+                    if (0 <= nx && nx < n && 0 <= ny && ny < n && ch[nx][ny] == 0) {
+                        Q.offer(new Graph(nx, ny, ob.cnt + 1));
+                        ch[nx][ny] = 1;
                     }
                 }
             }
-            L++;
         }
         return Integer.MAX_VALUE;
     }
 
-    public void change(int x, int y) {
+    public void naming(int x, int y) {
         Queue<Graph> Q = new LinkedList<>();
-        Q.offer(new Graph(x, y));
+        Q.offer(new Graph(x, y, 0));
         board[x][y] = num;
+        visit[x][y] = 1;
 
         while (!Q.isEmpty()) {
             Graph ob = Q.poll();
@@ -62,9 +70,10 @@ class Main {
                 int ny = ob.y + dy[i];
 
                 if (0 <= nx && nx < n && 0 <= ny && ny < n) {
-                    if (board[nx][ny] == 1) {
+                    if (board[nx][ny] == 1 && visit[nx][ny] == 0) {
+                        visit[nx][ny] = 1;
                         board[nx][ny] = num;
-                        Q.offer(new Graph(nx, ny));
+                        Q.offer(new Graph(nx, ny, 0));
                     }
                 }
             }
@@ -84,19 +93,20 @@ class Main {
             }
         }
 
+        visit = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == 1) {
-                    T.change(i, j);
+                if (board[i][j] == 1 && visit[i][j] == 0) {
+                    T.naming(i, j);
                 }
             }
         }
 
-        int answer = Integer.MAX_VALUE;
+        answer = Integer.MAX_VALUE;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] != 0) {
-                    answer = Math.min(answer, T.BFS(0, i, j, board[i][j]));
+                    answer = Math.min(answer, T.BFS(i, j, board[i][j]));
                 }
             }
         }
